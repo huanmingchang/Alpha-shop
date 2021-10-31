1d5793d51e98885cd6ba9d973e332ffc3c97c0e7
huanmingchang <huanming1988@gmail.com>
2021-10-31 13:11:19 +0800

    feat: add adjust qty function

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import './scss/main.scss'
 
 const hamburger = document.querySelector('.hamburger')
-
-
-
+const cartContainer = document.querySelector('.main__cart')
 
 // TODO  visibility 切換問題
 function hamburgerOnClick(e) {
@@ -21,4 +19,22 @@ function hamburgerOnClick(e) {
   }
 }
 
+function adjustCartQty(e) {
+  const plusQty = e.target.previousElementSibling
+  const minusQty = e.target.nextElementSibling
+  if (!e.target.classList.contains('btn')) {
+    return
+  }
+  if (e.target.classList.contains('btn-plus')) {
+    plusQty.innerText = Number(plusQty.innerText) + 1
+  }
+  if (e.target.classList.contains('btn-minus')) {
+    minusQty.innerText = Number(minusQty.innerText) - 1
+    if (Number(minusQty.innerText) < 0) {
+      minusQty.innerText = 0
+    }
+  }
+}
+
 hamburger.addEventListener('click', hamburgerOnClick)
+cartContainer.addEventListener('click', adjustCartQty)
