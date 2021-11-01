f6362bf2d70e1d33731387c845073f5098e36e15
huanmingchang <huanming1988@gmail.com>
2021-11-01 08:14:19 +0800

    feat: add calculateTotalAmount function

--- src/main.js
+++ src/main.js
@@ -34,6 +34,40 @@ function adjustCartQty(e) {
       minusQty.innerText = 0
     }
   }
+  calculateTotalAmount()
+}
+
+function calculateTotalAmount() {
+  const qtyArr = document.querySelectorAll(
+    '.main__cart-container__items__product-info-wrapper--modifier--qty'
+  )
+  const priceArr = document.querySelectorAll(
+    '.main__cart-container__items__product-info-wrapper--price'
+  )
+  const freight = Number(
+    document
+      .querySelector('.main__cart-container__items__freight--total')
+      .innerText.replace(/[^0-9]+/g, '')
+  )
+  const total = document.querySelector(
+    '.main__cart-container__items__subtotal--total'
+  )
+  const qty = []
+  const price = []
+  let sum = 0
+
+  qtyArr.forEach((element) => qty.push(Number(element.innerText)))
+  priceArr.forEach((element) =>
+    price.push(Number(element.innerText.replace(/[^0-9]+/g, '')))
+  )
+
+  for (let i = 0; i < qty.length; i++) {
+    sum += qty[i] * price[i]
+  }
+
+  sum += freight
+  total.innerText =
+    '$' + sum.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')
 }
 
 hamburger.addEventListener('click', hamburgerOnClick)
